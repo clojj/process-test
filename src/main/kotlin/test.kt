7ac7c9ea7e0a0629909cd272a6a046e9759eeb8a
Joerg Winter <joerg.winter@f-i-solutions-plus.de>
2018-10-24 14:14:26 +0200

    migrate to 1.3 + update to kotlinx.coroutines

--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -1,23 +1,23 @@
-import kotlinx.coroutines.experimental.channels.Channel
-import kotlinx.coroutines.experimental.channels.ReceiveChannel
-import kotlinx.coroutines.experimental.channels.produce
-import kotlinx.coroutines.experimental.runBlocking
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.runBlocking
 import okio.Buffer
 import org.zeroturnaround.exec.ProcessExecutor
 import java.io.File
-import kotlin.coroutines.experimental.CoroutineContext
+import kotlin.coroutines.CoroutineContext
 
 
 fun main(args: Array<String>) = runBlocking {
-/*
-    if (args.size < 1) {
-        println("Usage: $ main.kt COMMAND")
-        System.exit(1)
-        return@runBlocking
-    }
-    val (command, option) = args
-    println("Parsing $ man $command $option")
-*/
+    /*
+        if (args.size < 1) {
+            println("Usage: $ main.kt COMMAND")
+            System.exit(1)
+            return@runBlocking
+        }
+        val (command, option) = args
+        println("Parsing $ man $command $option")
+    */
 
     val producer = executeBashCommand(coroutineContext, "ping.exe", "localhost")
 
@@ -25,25 +25,17 @@ fun main(args: Array<String>) = runBlocking {
 
 }
 
-suspend fun searchForOption(option: String, producer: ReceiveChannel<String>) {
-    var foundBefore = false
+suspend fun searchForOption(text: String, producer: ReceiveChannel<String>) {
+    println("enter searchForOption")
     for (line in producer) {
         val words = line.splitToWords()
-        val tries = listOf(words.getOrNull(0), words.getOrNull(1))
-        val foundNow = tries.any { it?.startsWith(option) == true }
-        if (foundNow && line.isNotBlank()) println(line)
-/*
-        val hasArgument = tries.any { it?.startsWith("-") == true }
-        if (foundBefore && hasArgument) break
-        foundBefore = foundBefore or foundNow
-        if (foundBefore && line.isNotBlank()) println(line)
-*/
-
+        val found = words.any { s -> s.contains(text) }
+        if (found && line.isNotBlank()) println(line)
     }
 }
 
 fun executeBashCommand(context: CoroutineContext, command: String, vararg args: String) =
-    produce<String>(context, Channel.UNLIMITED) {
+    CoroutineScope(context).produce<String>(context, 42) {
 
         val allArgs = arrayOf(command, *args)
         Buffer().use { buffer ->
@@ -54,8 +46,8 @@ fun executeBashCommand(context: CoroutineContext, command: String, vararg args: 
                 .setMessageLogger { _, _, _ -> }
                 .execute()
             while (!buffer.exhausted()) {
-//                val line = buffer.readUtf8Line() ?: break
-                val line = buffer.readUtf8LineStrict() ?: break
+                val line = buffer.readUtf8Line() ?: break
+                println("read line: $line")
                 channel.send(line)
             }
         }
